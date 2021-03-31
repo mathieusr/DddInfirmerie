1ee448dff8f895d17b2c831f758937dbd15b10a3
nzaouesgi <hakhenaton@protonmail.com>
2021-03-31 15:15:19 +0200

    mvn project

---
+++ src/main/java/fr/esgi/ddd/infirmerie/App.java
@@ -0,0 +1,13 @@
+package fr.esgi.ddd.infirmerie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
