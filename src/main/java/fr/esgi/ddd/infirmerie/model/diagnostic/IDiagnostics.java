5be3b2392bf2a01c204da6c7aa4270d068f058a0
nzaouesgi <hakhenaton@protonmail.com>
2021-03-31 16:06:23 +0200

    added repository interfaces

---
+++ src/main/java/fr/esgi/ddd/infirmerie/model/diagnostic/IDiagnostics.java
@@ -0,0 +1,5 @@
+package fr.esgi.ddd.infirmerie.model.diagnostic;
+
+public interface IDiagnostics {
+    
+}
---
+++ src/main/java/fr/esgi/ddd/infirmerie/model/soins_internes/IMedicaments.java
@@ -0,0 +1,5 @@
+package fr.esgi.ddd.infirmerie.model.soins_internes;
+
+public interface IMedicaments {
+    
+}
