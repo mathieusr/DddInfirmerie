630ad14bc8a9991c4d16e26cf24c4d071e3c0b94
GautierL <gautier.laurent4@gmail.com>
2021-03-31 19:56:43 +0200

    add test fairediagnostic

--- src/test/java/fr/esgi/ddd/infirmerie/use_case/diagnostic/FaireDiagnosticTest.java
+++ src/test/java/fr/esgi/ddd/infirmerie/use_case/diagnostic/FaireDiagnosticTest.java
@@ -27,4 +27,40 @@ public class FaireDiagnosticTest {
 
         assertTrue("le résultat du diagnostic doit être être lié aux antécédents de l'élève.", resultat.isLinkedToAntecedants());
     }
+
+    @Test
+    public void should_diagnostic_student_with_harmless_symptoms(){
+        Eleve eleve = new Eleve();
+
+        FakeDiagnostics diagnostics = new FakeDiagnostics();
+
+        FaireDiagnostic faireDiagnostic = new FaireDiagnostic(diagnostics);
+        Diagnostic resultat = faireDiagnostic.diagnostiquer(eleve, EtatSante.BENIN);
+
+        assertTrue("le résultat du diagnostic doit être bénin", resultat.getEtatSante() == EtatSante.BENIN);
+    }
+
+    @Test
+    public void should_diagnostic_student_with_severe_symptoms(){
+        Eleve eleve = new Eleve();
+
+        FakeDiagnostics diagnostics = new FakeDiagnostics();
+
+        FaireDiagnostic faireDiagnostic = new FaireDiagnostic(diagnostics);
+        Diagnostic resultat = faireDiagnostic.diagnostiquer(eleve, EtatSante.GRAVE);
+
+        assertTrue("le résultat du diagnostic doit être grave", resultat.getEtatSante() == EtatSante.GRAVE);
+    }
+
+    @Test
+    public void should_diagnostic_student_with_psychological_symptoms(){
+        Eleve eleve = new Eleve();
+
+        FakeDiagnostics diagnostics = new FakeDiagnostics();
+
+        FaireDiagnostic faireDiagnostic = new FaireDiagnostic(diagnostics);
+        Diagnostic resultat = faireDiagnostic.diagnostiquer(eleve, EtatSante.PSYCHOLOGIQUE);
+
+        assertTrue("Le résultat du diagnostic doit être psychologique", resultat.getEtatSante() == EtatSante.PSYCHOLOGIQUE);
+    }
 }
